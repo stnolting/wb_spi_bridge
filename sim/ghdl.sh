de319d5f7b0af742873538d9d4379d4e603a47ff
stnolting <stnolting@gmail.com>
2021-11-29 06:14:52 +0100

    [sim] added GDHL script

---
+++ sim/ghdl.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Analyse sources
+ghdl -a ../rtl/wb_spi_bridge.vhd
+ghdl -a wb_spi_bridge_tb.vhd
+
+# Elaborate top entity
+ghdl -e wb_spi_bridge_tb
+
+# Run simulation
+ghdl -e wb_spi_bridge_tb
+ghdl -r wb_spi_bridge_tb --stop-time=1ms --vcd=wb_spi_bridge.vcd
